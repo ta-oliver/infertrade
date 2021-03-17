cae3a6b2c9a1589547b55a0227c9a29e44f44aff
Thomas o <sdfsdfsdfsd>
2021-03-17 14:05:21 +0000

    Partial fix to tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,7 @@ Created date: 11/03/2021
 from sklearn.pipeline import make_pipeline
 from ta.momentum import AwesomeOscillatorIndicator
 
-from infertrade.data import fake_market_data_4_years
+from infertrade.data.simulate_data import simulated_market_data_4_years_gen
 from infertrade.algos.community import normalised_close, scikit_signal_factory
 from infertrade.utilities.operations import PositionsFromPricePrediction, \
     PricePredictionFromPositions, PricePredictionFromSignalRegression
@@ -52,21 +52,22 @@ def test_run_atr_indicator(test_market_data_4_years):
 
 def test_transformers():
     pos_from_price = PositionsFromPricePrediction()
-    fake_market_data_4_years["forecast_price_change"] = fake_market_data_4_years["close"] * 0.000_1
-    df = pos_from_price.fit_transform(fake_market_data_4_years)
+    df = simulated_market_data_4_years_gen()
+    df["forecast_price_change"] = df["close"] * 0.000_1
+    df_with_positions = pos_from_price.fit_transform(df)
     predictions_from_positions = PricePredictionFromPositions()
     df0 = predictions_from_positions.fit_transform(df[["position"]])
     df0 = df0.round()
-    df = df.round()
+    df = df_with_positions.round()
     assert list(df["forecast_price_change"]) == list(df0["forecast_price_change"])
 
 
 def test_regression():
-    fake_market_data_4_years["signal"] = fake_market_data_4_years["close"].shift(-1)
-    print(fake_market_data_4_years)
+    simulated_market_data_4_years_gen["signal"] = simulated_market_data_4_years_gen["close"].shift(-1)
+    print(simulated_market_data_4_years_gen)
     price_predictin_from_signal = PricePredictionFromSignalRegression()
 
-    out = price_predictin_from_signal.fit_transform(fake_market_data_4_years)
+    out = price_predictin_from_signal.fit_transform(simulated_market_data_4_years_gen)
     print(out)
 
 
@@ -77,7 +78,7 @@ def test_pipeline_signal_to_position():
         PositionsFromPricePrediction()
     )
 
-    df = signal_to_positions.fit_transform(fake_market_data_4_years)
+    df = signal_to_positions.fit_transform(simulated_market_data_4_years_gen)
 
     print(df)
 
@@ -85,34 +86,32 @@ def test_pipeline_signal_to_position():
 def test_readme_example_one():
     """Example of signal generation from time series via simple function"""
     from infertrade.algos.community import normalised_close, scikit_signal_factory
-    from infertrade.data import fake_market_data_4_years
+    from infertrade.data import simulated_market_data_4_years_gen
     signal_transformer = scikit_signal_factory(normalised_close)
-    signal_transformer.fit_transform(fake_market_data_4_years)
+    signal_transformer.fit_transform(simulated_market_data_4_years_gen)
 
 
 def test_readme_example_one_external():
     """Example of signal generation from time series via simple function"""
     from infertrade.algos.community import scikit_signal_factory
-    from infertrade.data import fake_market_data_4_years
+    from infertrade.data import simulated_market_data_4_years_gen
     from infertrade.algos import ta_adaptor
     from ta.trend import AroonIndicator
     adapted_aroon = ta_adaptor(AroonIndicator, "aroon_down", window=1)
     signal_transformer = scikit_signal_factory(adapted_aroon)
-    signal_transformer.fit_transform(fake_market_data_4_years)
+    signal_transformer.fit_transform(simulated_market_data_4_years_gen)
 
 
 def test_readme_example_two():
     """Example of position calculation from simple position function"""
     from infertrade.algos.community.positions import constant_allocation_size, scikit_position_factory
-    from infertrade.data import fake_market_data_4_years
     position_transformer = scikit_position_factory(constant_allocation_size)
-    position_transformer.fit_transform(fake_market_data_4_years)
+    position_transformer.fit_transform(simulated_market_data_4_years_gen)
 
 
 def test_readme_example_three():
     """Get price prediction and positions from a signal transformer"""
     from infertrade.algos.community import normalised_close, scikit_signal_factory
-    from infertrade.data import fake_market_data_4_years
     from infertrade.utilities.operations import PositionsFromPricePrediction, \
         PricePredictionFromSignalRegression
     from sklearn.pipeline import make_pipeline
@@ -122,12 +121,11 @@ def test_readme_example_three():
                              PositionsFromPricePrediction()
                              )
 
-    pipeline.fit_transform(fake_market_data_4_years)
+    pipeline.fit_transform(simulated_market_data_4_years_gen)
 
 def test_readme_example_four():
     """Get price prediction and positions from an external signal transformer"""
     from infertrade.algos.community import scikit_signal_factory
-    from infertrade.data import fake_market_data_4_years
     from infertrade.utilities.operations import PositionsFromPricePrediction, \
         PricePredictionFromSignalRegression
     from sklearn.pipeline import make_pipeline
@@ -142,4 +140,4 @@ def test_readme_example_four():
                              PositionsFromPricePrediction()
                              )
 
-    pipeline.fit_transform(fake_market_data_4_years)
+    pipeline.fit_transform(simulated_market_data_4_years_gen)
