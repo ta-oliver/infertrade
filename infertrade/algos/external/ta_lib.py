a0804b8a672bbc298071b7ac38a23c6b7418b170
Dmytro Asieiev <66639835+DimaPes@users.noreply.github.com>
2021-03-18 15:10:52 +0300

    Adding ta_adapter

--- infertrade/algos/external/ta_lib.py
+++ infertrade/algos/external/ta_lib.py
@@ -4,24 +4,30 @@ Functions to facilitate usage of ta-lib functionality with infertrade interface
 Created by: Dmytro Asieiev
 Created date: 16/03/2021
 """
+
+import pandas as pd
 import talib
 from talib.abstract import Function
 from typing_extensions import Type
 
 
-
-def talib_adapter(function_mixin: str, **kwargs):
+def talib_adapter(function_mixin: str, function_name: str, **kwargs):
+    output_strings = Function(function_mixin).output_names
     column_strings = Function(function_mixin).input_names
     column_strings = list(column_strings.items())[0][1] #Gettng requiered columns from OrderedDict
-    # column_strings1 = Function(function_mixin).parameters
-    print(column_strings1)
-    def func(df):
-        column_inputs = {column_name: df[column_name] for column_name in column_strings}
-        print(column_inputs)
-        indicator = function_mixin(**column_inputs, **kwargs)
-        # indicator_callable = getattr(indicator)
-        # df["signal"] = indicator_callable()
-        df["signal"] = indicator
+    print(column_strings)
+
+    def func(df: pd.DataFrame) -> pd.DataFrame:
+        try:
+            output_index = output_strings.index(function_name)
+        except ValueError:
+            raise ValueError("Not existing function_name was supplied")
+        input_arrays = {column_name: df[column_name] for column_name in column_strings}
+        print(input_arrays)
+        indicator = Function(function_mixin)
+        result = indicator(input_arrays, **kwargs)
+        df["signal"] = result[output_index]
+
         return df
 
     return func
