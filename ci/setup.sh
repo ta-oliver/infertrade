197078e9de33a97381a8a8f84bd7a64092aa2673
Stephen Cairns <stephen.cairns@inferstat.com>
2021-04-13 17:28:57 +0100

    ci setup file

---
+++ ci/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Make sure we are using up to date Python 3.7
+sudo apt-get install python3.7-tk -y
+sudo apt-get install --only-upgrade python3.7-tk -y
+
+# Install ta-lib requirements
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+
+# Install other packages
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
