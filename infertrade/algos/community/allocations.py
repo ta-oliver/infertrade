82d806f6a31ff49e4041cc05fd3aa112e4a3722d
Fernando Azambuja <83773295+fernando-az-infertrade@users.noreply.github.com>
2021-05-19 15:44:58 -0300

    First draft

--- infertrade/algos/community/allocations.py
+++ infertrade/algos/community/allocations.py
@@ -36,6 +36,46 @@ def buy_and_hold(dataframe: pd.DataFrame) -> pd.DataFrame:
     return dataframe
 
 
+def chande_kroll_crossover_strategy(
+        dataframe: pd.DataFrame,
+        allow_short_selling: bool = False
+        ) -> pd.DataFrame:
+    """
+    This simple all-or-nothing rule:
+    (1) allocates 100% of the portofolio to a long position on the asset when the price of the asset is above both the
+    Chande Kroll stop long line and Chande Kroll stop short line, and
+    (2) according to the value set for the allow_short_selling parameter, either allocates 0% of the portofiolio to
+    the asset or allocates 100% of the portfolio to a short position on the asset when the price of the asset is below
+    both the Chande Kroll stop long line and the Chande Kroll stop short line.
+
+    parameters:
+    allow_short_selling: allow taking short positions on the portfolio.
+    """
+    # Calculate the Chande Kroll lines, which will be added to the DataFrame as columns named "chande_kroll_long" and
+    # "chande_kroll_short".
+    import infertrade.algos.community.signals
+    import copy
+    dataframe = infertrade.algos.community.signals.chande_kroll(dataframe)
+
+    # Allocate positions according to the Chande Kroll lines
+    is_price_above_lines = (
+            (dataframe["price"] > dataframe["chande_kroll_long"]) &
+            (dataframe["price"] > dataframe["chande_kroll_short"])
+            )
+    is_price_below_lines = (
+            (dataframe["price"] < dataframe["chande_kroll_long"]) &
+            (dataframe["price"] < dataframe["chande_kroll_short"])
+            )
+
+    dataframe.loc[is_price_above_lines, PandasEnum.ALLOCATION.value] = 1.0
+    dataframe.loc[is_price_below_lines, PandasEnum.ALLOCATION.value] = -1.0 if allow_short_selling else 0.0
+
+    # Delete the columns with the Chande Kroll indicators before returning
+    dataframe.drop(columns=["chande_kroll_long", "chande_kroll_short"], inplace=True)
+
+    return dataframe
+
+
 def constant_allocation_size(dataframe: pd.DataFrame, fixed_allocation_size: float = 1.0) -> pd.DataFrame:
     """
     Returns a constant allocation, controlled by the fixed_allocation_size parameter.
@@ -134,6 +174,14 @@ infertrade_export_allocations = {
         "series": [],
         "available_representation_types": {
             "github_permalink": "https://github.com/ta-oliver/infertrade/blob/b2cf85c28ed574b8c246ab31125a9a5d51a8c43e/infertrade/algos/community/allocations.py#L34"
+        },
+    },
+    "chande_kroll_crossover_strategy": {
+        "function": chande_kroll_crossover_strategy,
+        "parameters": {"allow_short_selling": False},
+        "series": [],
+        "available_representation_types": {
+            "github_permalink": "" # TODO XXX add permalink
         },
     },
     "constant_allocation_size": {
--- infertrade/algos/community/signals.py
+++ infertrade/algos/community/signals.py
@@ -24,6 +24,10 @@ import numpy as np
 from sklearn.preprocessing import FunctionTransformer
 from infertrade.data.simulate_data import simulated_market_data_4_years_gen
 
+from ta.volatility import AverageTrueRange
+from infertrade.algos.external.ta import ta_adaptor
+from infertrade.PandasEnum import PandasEnum
+
 
 def normalised_close(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -45,6 +49,46 @@ def high_low_diff(df: pd.DataFrame) -> pd.DataFrame:
 def high_low_diff_scaled(df: pd.DataFrame, amplitude: float = 1) -> pd.DataFrame:
     """Example signal based on high-low range times scalar."""
     df["signal"] = (df["high"] - max(df["low"])) * amplitude
+    return df
+
+
+def chande_kroll(
+        df: pd.DataFrame,
+        average_true_range_periods: int = 10,
+        average_true_range_multiplier: float = 1.0,
+        stop_periods: int = 9
+        ) -> pd.DataFrame:
+    # TODO XXX change description
+    """Example signal based on high-low range times scalar."""
+
+    # Calculate the maximum and minum prices that the asset attained in the last
+    # average_true_range_periods periods
+    max_in_n_periods = df["high"].rolling(window=average_true_range_periods).max()
+    min_in_n_periods = df["low"].rolling(window=average_true_range_periods).min()
+
+    # Calculate the Average True Range indicator using average_true_range_periods periods, and
+    # temporarily store those values in df[PandasEnum.SIGNAL.value]
+    adapted_average_true_range = ta_adaptor(
+            AverageTrueRange,
+            "average_true_range",
+            window=average_true_range_periods,
+            fillna=False
+            )
+
+    signal_transformer = scikit_signal_factory(adapted_average_true_range)
+    signal_transformer.fit_transform(df)
+
+    # Calculate the intermediate high and low stops
+    intermediate_high_stops = max_in_n_periods - (df[PandasEnum.SIGNAL.value] * average_true_range_multiplier)
+    intermediate_low_stops = min_in_n_periods + (df[PandasEnum.SIGNAL.value] * average_true_range_multiplier)
+
+    # Obtain the stop long and stop short values
+    df["chande_kroll_long"] = intermediate_high_stops.rolling(window=stop_periods).max()
+    df["chande_kroll_short"] = intermediate_low_stops.rolling(window=stop_periods).min()
+
+    # Drop the Average True Range indicator values before returning
+    df.drop(columns=PandasEnum.SIGNAL.value, inplace=True)
+
     return df
 
 
@@ -77,6 +121,14 @@ infertrade_export_signals = {
         "series": ["high", "low"],
         "available_representation_types": {
             "github_permalink": "https://github.com/ta-oliver/infertrade/blob/5aa01970fc4277774bd14f0823043b4657e3a57f/infertrade/algos/community/signals.py#L45"
+        },
+    },
+    "chande_kroll": {
+        "function": chande_kroll,
+        "parameters": {"average_true_range_periods": 10, "average_true_range_multiplier": 1.0, "stop_periods": 9},
+        "series": ["high", "low"],
+        "available_representation_types": {
+            "github_permalink": "" # TODO XXX add permalink
         },
     },
 }
