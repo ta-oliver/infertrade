f9d51a90333a50b75216fc6cebbb4d02f83f4559
Thomas o <sdfsdfsdfsd>
2021-08-23 12:26:58 +0100

    Make function creation use deepcopy

--- infertrade/algos/community/ta_regressions.py
+++ infertrade/algos/community/ta_regressions.py
@@ -1,3 +1,20 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2021 InferStat Ltd
+# Created by: Thomas Oliver
+# Created date: 23/08/2021
+
+
 import pandas as pd
 from sklearn.preprocessing import FunctionTransformer
 
@@ -9,6 +26,7 @@ from infertrade.utilities.operations import (
 )
 from sklearn.pipeline import make_pipeline
 from infertrade.algos.external.ta import ta_adaptor
+from copy import deepcopy
 
 
 def ta_rules_with_regression() -> dict:
@@ -34,11 +52,16 @@ def ta_rules_with_regression() -> dict:
             time_series_df = pipeline.fit_transform(time_series_df)
             return time_series_df
 
+	def create_allocation_function():
+            """Creates an allocation function."""
+            return deepcopy(allocation_function)
+
+
         ta_regression_name = ta_rule_name + "_regression"
 
         dictionary_addition = {
             ta_regression_name: {
-                "function": allocation_function,
+                "function": create_allocation_function(),
                 "parameters": ta_export_signals[ii_ta_signal]["parameters"],
                 "series": ta_export_signals[ii_ta_signal]["series"],
                 "available_representation_types": {
