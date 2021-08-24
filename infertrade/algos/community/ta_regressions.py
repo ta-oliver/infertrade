ef8f5142e29f16c8665d9606f42adcc7e6050ff5
bi-kash <bkash.timsina@gmail.com>
2021-08-24 11:42:46 +0545

    ABC metaerror solve and add dynamic infertrade permalink

--- infertrade/algos/community/ta_regressions.py
+++ infertrade/algos/community/ta_regressions.py
@@ -31,15 +31,13 @@ from infertrade.utilities.operations import (
 from infertrade.algos.external.ta import ta_adaptor
 
 
-def create_allocation_function(ta_signal_name: str, ta_raw_signal_func: Callable) -> Callable[[pd.DataFrame],
-                                                                                              pd.DataFrame]:
+def create_allocation_function(ta_signal_name: str, ta_raw_signal_func: Callable) -> Callable:
     """Creates an allocation function for a ta library signal by regression against the time step price changes."""
 
     def allocation_function(time_series_df: pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
         """Generic regression treatment of ta technical signals."""
-        ta_signal_func = ta_export_signals[ta_raw_signal_func]["class"]
 
-        adapted_allocation_rule_using_regression = ta_adaptor(ta_signal_func, ta_signal_name, *args, **kwargs)
+        adapted_allocation_rule_using_regression = ta_adaptor(ta_raw_signal_func, ta_signal_name, *args, **kwargs)
 
         pipeline = make_pipeline(
             FunctionTransformer(adapted_allocation_rule_using_regression),
@@ -52,6 +50,8 @@ def create_allocation_function(ta_signal_name: str, ta_raw_signal_func: Callable
 
     return allocation_function
 
+
+github_permalink = "https://github.com/ta-oliver/infertrade/blob/e45fe2b76f9a16c07c48a6739e3173737cb5355c/infertrade/algos/community/ta_regressions.py"
 
 def ta_rules_with_regression() -> dict:
     """Creates an equivalent dictionary of allocation rules by regressing signals against future price changes."""
@@ -71,7 +71,7 @@ def ta_rules_with_regression() -> dict:
                 "parameters": ta_export_signals[ii_ta_signal]["parameters"],
                 "series": ta_export_signals[ii_ta_signal]["series"],
                 "available_representation_types": {
-                    "github_permalink": "https://github.com/ta-oliver/infertrade/blob/f571d052d9261b7dedfcd23b72d925e75837ee9c/infertrade/algos/community/allocations.py#L282"
+                    "github_permalink": github_permalink + "#L" + str(create_allocation_function.__code__.co_firstlineno)
                 },
             }
         }
