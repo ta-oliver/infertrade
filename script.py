232d2bdaba10ff7556ca08353c8bb4054b040a6c
asifnawaz24 <asif_nawaz96@live.com>
2021-08-29 22:11:41 +0500

    delete script.py

--- script.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-    btc_df = pd.read_csv('./examples/BTC.csv')
-    lbma_df = pd.read_csv('./examples/LBMA_Gold.csv')
-    print(btc_df.head)
-    print(lbma_df.head)
