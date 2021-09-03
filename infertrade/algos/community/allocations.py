4a89cd6686895a92f444c5c9847ee2cb1fdd884c
bi-kash <bkash.timsina@gmail.com>
2021-09-03 12:12:13 +0545

    remove function

--- infertrade/algos/community/allocations.py
+++ infertrade/algos/community/allocations.py
@@ -949,7 +949,6 @@ def create_infertrade_export_allocations():
         infertrade_export_allocations_raw.update(
             {
                 function.__name__: {
-                    "function": function,
                     "parameters": get_parameters(function),
                     "series": series_dict[function.__name__ + "_series"],
                     "available_representation_types": {"github_permalink": create_permalink_to_allocations(function)},
